b99a14f4024d2e89b9cb7d9e1fff738a9a75588c
Nelson Nwajie <nwajienelson@gmail.com>
2023-12-23 18:07:06 -0700

    Update customizeScript2.sh

--- quickquickstarts/customizeScript2.sh
+++ quickquickstarts/customizeScript2.sh
@@ -2,38 +2,12 @@
 
 sudo su -
 
-USER1=imagebuilder
+USER1=mlopsserver
 sudo useradd -m $USER1
 sudo usermod -aG sudo $USER1
 
-# kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-# helm
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-
-
-# docker
-sudo apt-get update 
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-key fingerprint 0EBFCD88
 sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo usermod -aG docker $USER 
-sudo chmod 777 /var/run/docker.sock
-#sudo su - $USER
-
-
-#####################
-# install azure cli #
-#####################
-
-sudo apt-get update
 sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg -y
 sudo mkdir -p /etc/apt/keyrings
 curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
@@ -49,22 +23,54 @@ sudo apt-get update -y
 sudo apt-get install azure-cli -y
 
 
-# kustomize
+# install kubectl
+curl -LO https://dl.k8s.io/release/v1.26.0/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
 
-wget https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.1.1/kustomize_v5.1.1_linux_amd64.tar.gz
-tar -xf kustomize_v5.1.1_linux_amd64.tar.gz
-sudo cp kustomize /usr/local/bin
 
-#python
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update -y
-sudo apt-get install python3.10 -y
-sudo apt install ansible -y
+# install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# install kustomize
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+sudo mv ~/kustomize /usr/local/bin
+
+# go
+wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+#pip and venv
+
+sudo apt update -y
+yes | sudo apt install python3-pip
+yes | sudo apt install -y python3-venv
+
+# java 11
+sudo apt update -y
+sudo apt install openjdk-11-jdk -y
+
+# istioctl
+curl -L https://istio.io/downloadIstio | sh -
+export PATH="$PATH:/home/mlopsserver/istio-1.20.1/bin"
+
+# mysql client
+sudo apt update -y
+sudo apt install mysql-server
+
+#jenkins
+wget -P ~/jenkins/ https://get.jenkins.io/war-stable/2.426.2/jenkins.war
+# java -jar jenkins.war --httpPort=9090
+
 
 # terraform
 
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+sudo apt-get update -y && sudo apt-get install -y gnupg software-properties-common
 wget -O- https://apt.releases.hashicorp.com/gpg | \
 gpg --dearmor | \
 sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
@@ -74,7 +80,19 @@ gpg --no-default-keyring \
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
 https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update
+sudo apt update -y
 sudo apt-get install terraform -y
 
-sudo su - $USER1
+# docker
+sudo apt-get update -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+yes | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" 
+sudo apt-key fingerprint 0EBFCD88
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo usermod -aG docker $USER 
+sudo chmod 777 /var/run/docker.sock
+sudo chown -R $USER /var/run/docker.sock
+sudo su - $USER
+
